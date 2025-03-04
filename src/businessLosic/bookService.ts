7971022c1174f816d3956a39d6693e92072609da
tom106gunma <tom106.net@gmail.com>
2025-03-05 07:19:15 +0900

    ビジネスロジック層の実装

---
+++ src/businessLosic/bookService.ts
@@ -0,0 +1,18 @@
+import { Book } from "@prisma/client";
+import { PrismaBookRepository } from "../dataAccess/PrismaBookRepository";
+
+export class BookService {
+  private bookRepository: PrismaBookRepository;
+
+  constructor() {
+    this.bookRepository = new PrismaBookRepository();
+  }
+
+  async add(title: string): Promise<Book> {
+    return await this.bookRepository.create(title);
+  }
+
+  async find(id: string): Promise<Book | null> {
+    return await this.bookRepository.findById(id);
+  }
+}
