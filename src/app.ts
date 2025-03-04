70c2b0082620cc72ab3cf270394cf8f5dca4e5fa
tom106gunma <tom106.net@gmail.com>
2025-03-05 08:10:21 +0900

    エントリーポイントの修正

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
+import { BookController } from "./presentation/bookController";
 
 const app = express();
 app.use(express.json());
 
+const bookController = new BookController();
+
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.json({message: 'Hello world!'});
-});
+app.post('/books', bookController.add.bind(bookController));
+app.get('/books/:id', bookController.findById.bind(bookController));
 
 app.listen(PORT, () => console.log('Server is running'));
